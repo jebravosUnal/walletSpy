2b74a95d587c4fd43f4871d65dfe49dde4131f18
esteban <esteban.bravo@viseo.com>
2017-06-13 15:17:04 +0200

    configInit -> dependencies SLF4J, spring boot

---
+++ WalletSpyApp/src/main/java/WalletApp.java
@@ -0,0 +1,11 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by EBR3556 on 13/06/2017.
+ */
+//@SpringBootApplication
+public class WalletApp {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(WalletApp.class);
+}
