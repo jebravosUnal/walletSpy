fc2774ad2bf7de70d7d235695363a854ffc5215c
Esteban Bravo <jebravos@unal.edu.co>
2017-06-13 16:24:41 +0200

    configInit -> dependencies SLF4J, spring boot

--- WalletSpyApp/src/main/java/WalletApp.java
+++ WalletSpyApp/src/main/java/WalletApp.java
@@ -1,11 +1,26 @@
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 /**
  * Created by EBR3556 on 13/06/2017.
  */
 //@SpringBootApplication
-public class WalletApp {
+public class WalletApp extends SpringBootServletInitializer {
 
-    private final Logger LOGGER = LoggerFactory.getLogger(WalletApp.class);
+    private final static Logger LOGGER = LoggerFactory.getLogger(WalletApp.class);
+    private static Class<WalletApp> applicationClass = WalletApp.class;
+
+    public static void main(String[] args) {
+        SpringApplication.run(WalletApp.class, args);
+        LOGGER.debug("Hola mundo desde wallet app");
+    }
+
+
+
+//    Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//        return application.sources(applicationClass);
+//    }
 }
